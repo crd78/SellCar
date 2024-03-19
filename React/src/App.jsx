ceba5cd4421c92ef0aefd569ad9439255fcd8aa7
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-19 16:54:25 +0100

    debut des routes

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,44 +1,15 @@
-import { useState, useEffect } from 'react';
-import '../src/asset/css/VehiculeList.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import VehiculeList from './asset/pages/VehiculeList';
 
 function App() {
-  const [models, setModels] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/modele');
-        const data = await response.json();
-        setModels(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div>
-      <h1 className="modele-list-title">Liste des modèles</h1>
-      <div className="modele-list-container">
-        {models.length > 0 ? (
-          models.map((model,) => (
-            <div key={model.id} className={`modele-item`}>
-              <h2 className={`marque`} title={`Rechercher d'autre modèle de cette marque`}>{model.Marque}</h2>
-              <img src={model.photo} alt={model.photo} />
-              <div className="Tarif">{model.tarif} €</div>
-                <div className="details">
-                  <h3> Modèle : {model.model}</h3>
-                  <h3> Année : {model.année}</h3>
-                  <p>{model.description}</p>
-                </div>
-            </div>
-          ))
-        ) : (
-          <p>Aucun modèle trouvé</p>
-        )}
-      </div>
+    <div className='App'>
+      <Router>
+        <Routes>
+         
+          <Route path="/vehicule" element={<VehiculeList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ React/src/asset/pages/VehiculeList.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+
+
+function VehiculeList() {
+  const [models, setModels] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/modele');
+        const data = await response.json();
+        setModels(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="modele-list-title">Liste des modèles</h1>
+      <div className="modele-list-container">
+        {models.length > 0 ? (
+          models.map((model,) => (
+            <div key={model.id} className={`modele-item`}>
+              <h2 className={`marque`} title={`Rechercher d'autre modèle de cette marque`}>{model.Marque}</h2>
+              <img src={model.photo} alt={model.photo} />
+              <div className="Tarif">{model.tarif} €</div>
+                <div className="details">
+                  <h3> Modèle : {model.model}</h3>
+                  <h3> Année : {model.année}</h3>
+                  <p>{model.description}</p>
+                </div>
+            </div>
+          ))
+        ) : (
+          <p>Aucun modèle trouvé</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default VehiculeList
