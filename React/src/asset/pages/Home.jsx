96e74dd2c070310477967ff933b9000619c12564
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-21 19:20:29 +0100

    gammevehicule

--- React/src/asset/pages/Home.jsx
+++ React/src/asset/pages/Home.jsx
@@ -1,5 +1,6 @@
 import '../css/Home.css';
 
+
 const Home = () => {
     return (
         <div className='bodyHome'>
@@ -10,13 +11,20 @@ const Home = () => {
             <div className='contentHome'>
                 <h2 className='h2Home'> Un plaisir d`acheter chez nous </h2>
                 <p className='pHome'> Chez SellCar, nous mettons un point d`honneur à offrir un service client exceptionnel. Notre équipe dévouée est là pour répondre à toutes vos questions, vous guider dans votre processus d`achat et vous assurer une expérience agréable et sans tracas. Nous sommes là pour vous aider à trouver la voiture de vos rêves et nous nous engageons à vous offrir un service personnalisé et de qualité. Faites confiance à SellCar pour un service client inégalé. </p>
-                <h2 className='h2Home'> Une large gamme de véhicules </h2>
-                
-                <div className='contentGamme'>
-                    <h3 className='h3Home'> SUV </h3>
-                    <p className='pGamme'> Nos meilleurs modeles de SUV disponible à ce jour.</p>
-                    <img className='imgGamme' src='../src/asset/images/gammesuv.jpg'/>
-                    <img className='imgGamme2' src='../src/asset/images/gammesuv2.jpg'/>
+                <h2 className='h2Home'> Notre spécialité </h2>
+                <div className='gammeContainer'>
+                    <div className='contentGamme'>
+                        <h3 className='h3Home'> SUV </h3>
+                        <p className='pGamme'> Nos meilleurs modeles de SUV disponible à ce jour.</p>
+                        <img className='imgGamme' src='../src/asset/images/gammesuv.jpg'/>
+                        <img className='imgGamme2' src='../src/asset/images/gammesuv2.jpg'/>
+                    </div>
+                    <div className='contentGamme'>
+                        <h3 className='h3Home'> Berline </h3>
+                        <p className='pGamme'> Nos meilleurs modeles de Berline disponible à ce jour.</p>
+                        <img className='imgGamme' src='../src/asset/images/gammeberline.jpg'/>
+                        <img className='imgGamme2' src='../src/asset/images/gammeberline2.jpg'/>
+                    </div>
                 </div>
             </div>
         </div>
