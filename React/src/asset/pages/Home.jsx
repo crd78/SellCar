d373a230edd3b34d234e674dbfd96cbb41fdb8b6
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-21 19:21:24 +0100

    faute ortographe

--- React/src/asset/pages/Home.jsx
+++ React/src/asset/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
             <div className='contentHome'>
                 <h2 className='h2Home'> Un plaisir d`acheter chez nous </h2>
                 <p className='pHome'> Chez SellCar, nous mettons un point d`honneur à offrir un service client exceptionnel. Notre équipe dévouée est là pour répondre à toutes vos questions, vous guider dans votre processus d`achat et vous assurer une expérience agréable et sans tracas. Nous sommes là pour vous aider à trouver la voiture de vos rêves et nous nous engageons à vous offrir un service personnalisé et de qualité. Faites confiance à SellCar pour un service client inégalé. </p>
-                <h2 className='h2Home'> Notre spécialité </h2>
+                <h2 className='h2Home'> Nos spécialités </h2>
                 <div className='gammeContainer'>
                     <div className='contentGamme'>
                         <h3 className='h3Home'> SUV </h3>
